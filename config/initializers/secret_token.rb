df9fb89a098ed66e07d24b8c26122c94bc7cc6d6
bek <obozrevatel90@gmail.com>
2012-01-28 14:15:46 +0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Laima::Application.config.secret_token = '349472d2f244a7636a0c3b87cab60eb3364077557c7a21243b90e7f7850f0250a93b183057f6c1876a9cac107473b56df96b45989399856714fc689fdac520b9'
