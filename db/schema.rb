bf9c4a80653a0d878f844fdd23145f10622093e3
Mons Writer <monswriter@gmail.com>
2012-01-30 16:04:17 +0600

    Users (beginning)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120130082728) do
+ActiveRecord::Schema.define(:version => 20120130091837) do
 
   create_table "jobs", :force => true do |t|
     t.string   "title"
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(:version => 20120130082728) do
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
   end
 
 end
