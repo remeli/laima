c985f3a19e5b900b69d4e1b5a3ec4f8588dbde61
bek <obozrevatel90@gmail.com>
2012-01-30 14:48:24 +0600

    add views for clients

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,12 @@
 class Client < ActiveRecord::Base
   validates :title, :description, :presence => true
   has_attached_file :logo
+  
+  def button_value
+    if new_record?
+      "Добавить"
+    else
+      "Редактировать"      
+    end    
+  end
 end
