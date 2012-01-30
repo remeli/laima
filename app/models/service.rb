8fd623894b09437c220710ac4ba4d440700b0afc
Mons Writer <monswriter@gmail.com>
2012-01-30 14:54:01 +0600

    modified model service.rb (added attachment)

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,8 +2,14 @@
 
 class Service < ActiveRecord::Base
   
+  has_attached_file :photo, :styles => { :small => "150x150>" },
+                    :url => "/assets/services/:id/:style/:basename.:extension",
+                    :path => ":rails_root/public/assets/services/:id/:style/:basename.:extension"
+  
   validates :title, :presence => true, :length => {:maximum => 140}
   validates :description, :presence => true, :length => {:maximum => 800}
+  validates :short_description, :presence => true, :length => {:maximum => 250}
+  
   
   def button_value
     if new_record?
