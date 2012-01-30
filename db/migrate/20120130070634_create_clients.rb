bf447500b4bc430429f61877d2234d42af61bcb8
bek <obozrevatel90@gmail.com>
2012-01-30 13:31:25 +0600

    db migrate and create index.html

--- db/migrate/20120130070634_create_clients.rb
+++ db/migrate/20120130070634_create_clients.rb
@@ -1,6 +1,8 @@
 class CreateClients < ActiveRecord::Migration
   def change
     create_table :clients do |t|
+      t.string :title
+      t.text :description
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120128103727) do
+ActiveRecord::Schema.define(:version => 20120130070634) do
+
+  create_table "clients", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "services", :force => true do |t|
     t.string   "title"
