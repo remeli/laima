46f298dd3f6d99be8dd5cd1b8878085a22692e4b
Yerlan Kussainov <lagox.mail@gmail.com>
2012-01-30 14:33:33 +0600

    job form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  
+  def errors_for_object(object)
+    if object.errors.any?
+      render(:partial => "shared/errors", :locals => {:object => object})  
+    end
+  end
+  
 end
