92e0ecb038e3d1397f3abb8d33a6cca03bc693c6
bek <obozrevatel90@gmail.com>
2012-01-31 11:52:17 +0600

    modified views

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -4,13 +4,11 @@ class ClientsController < ApplicationController
   
   def index
     @clients = Client.page(params[:page]).per(7)
-    @title = @clients.title
     respond_with @clients   
   end
   
   def show
     @client = Client.find(params[:id])
-    @title = @clients.title
     respond_with @client 
   end
 end
