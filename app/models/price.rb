6016faa7e33001a8c1c005dca7dd293c41be8e86
bek <obozrevatel90@gmail.com>
2012-02-01 19:45:38 +0600

    validates numericality

--- app/models/price.rb
+++ app/models/price.rb
@@ -2,6 +2,7 @@
 class Price < ActiveRecord::Base
   belongs_to :service
   validates :price, :service_id, :presence => true
+  validates :price, :numericality => { :greater_than_or_equal_to=>0.01}
   
   def button_value
     if new_record?
