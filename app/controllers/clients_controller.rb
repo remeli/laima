7631042784cd09b71ec5fa0807b79cd0fb30e208
Yerlan Kussainov <lagox.mail@gmail.com>
2012-02-01 19:31:18 +0600

    index clients views

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -3,7 +3,7 @@ class ClientsController < ApplicationController
   respond_to :html
   
   def index
-    @clients = Client.page(params[:page]).per(7)
+    @clients = Client.page(params[:page]).per(10)
     respond_with @clients   
   end
   
