b6a97d5955365418fc69a9eda747c032fb5c521f
Yerlan Kussainov <lagox.mail@gmail.com>
2012-02-03 12:29:00 +0600

    models and attrbiutes transalte(russian)

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,7 @@
 #encoding: UTF-8
 class Client < ActiveRecord::Base
   validates :title, :presence => true
+  
   has_attached_file :logo, 
                     :styles => { :medium => "300x300>", :thumb => "120x80>" },
                     :url => "/system/:attachment/:id/:style/:basename.:extension",
@@ -8,7 +9,9 @@ class Client < ActiveRecord::Base
   validates_attachment_presence :logo
   validates_attachment_size :logo, :less_than => 5.megabytes
   validates_attachment_content_type :logo, :content_type => ['image/jpeg', 'image/png', 'image/gif']
+  
   default_scope order('title ASC')    
+  
   def button_value
     if new_record?
       "Добавить"
