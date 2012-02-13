7885ac88a60c5437f4f2fb80ee8fec7e8c584c10
Yerlan Kussainov <lagox.mail@gmail.com>
2012-02-13 16:26:29 +0600

    price

--- app/models/price.rb
+++ app/models/price.rb
@@ -1,8 +1,7 @@
 #encoding: UTF-8
 class Price < ActiveRecord::Base
   belongs_to :service
-  validates :price, :service_id, :presence => true
-  validates :price, :numericality => { :greater_than_or_equal_to=>0.01}
+  validates :service_id, :presence => true
   validates :service_id, :uniqueness => true
   
   def button_value
